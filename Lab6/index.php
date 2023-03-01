c73523afb9e0b7db49399c9f2044b3a45fab3120
samcollins4all <c.okoye2@rgu.ac.uk>
2023-03-01 09:48:14 +0000

    new l

---
+++ Lab6/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    </head>
+    <body>
+    <p>
+        <?php
+           echo "Hello World";
+                "How are you"
+           ?>
+          </p>
+</body>
+</html>
